da4bf08181b5d0781e1686cc6f7c09fe1761624f
DiracSpace <www.ralphlaurenpolo@gmail.com>
2020-09-13 23:20:50 -0500

    added ruby, js and haskell solutions

---
+++ Is_this_a_triangle/solution_ruby.rb
@@ -0,0 +1,3 @@
+def isTriangle(a,b,c)
+   return ((a + b) > c && (b + c) > a && (a + c) > b) ? true : false;
+end
---
+++ Multiply/solution_haskell.hs
@@ -0,0 +1,4 @@
+module Multiply.Bug.Fix where
+
+multiply :: Integer -> Integer -> Integer
+multiply a b =  a * b
