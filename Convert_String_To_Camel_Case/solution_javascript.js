774e81602bf096aec048ef6ddb06db047e4f84d6
DiracSpace <json.roberto.deleon@gmail.com>
2021-09-05 23:42:00 -0500

    mend

--- Convert_String_To_Camel_Case/solution_javascript.js
+++ Convert_String_To_Camel_Case/solution_javascript.js
@@ -11,19 +11,21 @@ function toCamelCase(str) {
             return s[0].toUpperCase() + s.slice(1);
         }
     });
-    
+
     return capitalizedLettersArray.join("");
 }
 
 // Reduced variables solution
-function toCamelCase2(str) {
+function toCamelCase(str) {
     if (str.length === 0) return '';
 
-    return (str.includes("-")) ? str.split("-") : str.split("_").map((s, i) => {
-        if (i === 0) {
-            return s[0] + s.slice(1);
-        } else {
-            return s[0].toUpperCase() + s.slice(1);
-        }
-    }).join("");
+    return ((str.includes("-"))
+        ? str.split("-")
+        : str.split("_")).map((s, i) => {
+            if (i === 0) {
+                return s[0] + s.slice(1);
+            } else {
+                return s[0].toUpperCase() + s.slice(1);
+            }
+        }).join("");
 }
