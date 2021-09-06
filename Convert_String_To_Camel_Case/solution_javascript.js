1eac3446bf50c85e074f4d2e9b63993ac9af3c1d
DiracSpace <json.roberto.deleon@gmail.com>
2021-09-05 23:37:38 -0500

    mend

---
+++ Convert_String_To_Camel_Case/solution_javascript.js
@@ -0,0 +1,29 @@
+// original solution with relative clean code conventions
+function toCamelCase(str) {
+    if (str.length == 0) return '';
+
+    const splitted = (str.includes("-")) ? str.split("-") : str.split("_");
+
+    const capitalizedLettersArray = splitted.map((s, i) => {
+        if (i === 0) {
+            return s[0] + s.slice(1);
+        } else {
+            return s[0].toUpperCase() + s.slice(1);
+        }
+    });
+    
+    return capitalizedLettersArray.join("");
+}
+
+// Reduced variables solution
+function toCamelCase2(str) {
+    if (str.length === 0) return '';
+
+    return (str.includes("-")) ? str.split("-") : str.split("_").map((s, i) => {
+        if (i === 0) {
+            return s[0] + s.slice(1);
+        } else {
+            return s[0].toUpperCase() + s.slice(1);
+        }
+    }).join("");
+}
